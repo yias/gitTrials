f60d50b90b4dd65de3a3ba7f6bc4147f3807494a
Iason Batzianoulis <iasonbatz@gmail.com>
2021-10-05 15:18:19 +0200

    first commit

---
+++ testpy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+    developer: Iason Batzianoulis
+    maintaner: Iason Batzianoulis
+    email: iason.batzianoulis@ansys.com
+    description:
+    process of creating a VM and then deleting it
+"""
+
+
+import numpy as np
+
+
+def fun(a, b):
+    print( np.sum( np.array([a, b]) ) )
+
+
+fun(1,3)
